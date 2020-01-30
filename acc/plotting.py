ea9da3b28dd43377c6079c4cfcef1adca1ac8113
sun031 <weijia_sun@163.com>
2020-01-30 19:44:22 +0800

    update plotting

--- acc/plotting.py
+++ acc/plotting.py
@@ -3,13 +3,28 @@
 Plottings
 """
 import matplotlib.pyplot as plt
-from matplotlib.ticker import (AutoMinorLocator, FixedLocator, FixedFormatter, MaxNLocator)
+from matplotlib.ticker import (FormatStrFormatter, MultipleLocator, AutoMinorLocator, FixedLocator, FixedFormatter, MaxNLocator)
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from matplotlib.markers import MarkerStyle
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
 from acc.stack import linear_stack
+from acc.profile import profile, get_profile_boxes
+from acc.io import _load_json
+from acc.migration import mig_one_station
+
 import numpy as np
 import warnings
 from obspy import Stream
+from obspy import read
+from obspy.geodetics import gps2dist_azimuth
+
 from collections import OrderedDict
-from acc.migration import mig_one_station
+
+import os
+import glob
+import math
 
 
 def plot_acc_one_station(stream, fname=None, fig_width=7., trace_height=0.5,
@@ -171,5 +186,208 @@ def plot_ppoint(stream, fname="pp.pdf", depths=[30, 50, 80, 100, 150, 200]):
 
     plt.tight_layout()
     plt.savefig(fname)
+
+
+
+def plot_profile(jsonfile):
+
+    kwargs = _load_json(jsonfile)
+    paras = kwargs["profile"]
+
+    latlon0 = paras["latlon0"]
+    if latlon0 is None:
+        # for a single station
+        azimuth = 0
+        dist = 400
+    else:
+        # for profile
+        latlon1 = paras["latlon1"]
+        lat1 = latlon0[0]
+        lon1 = latlon0[1]
+        lat2 = latlon1[0]
+        lon2 = latlon1[1]
+        dist, azimuth, baz = gps2dist_azimuth(lat1, lon1, lat2, lon2)
+        dist /= 1000  # from m to km
+        # padding 100 km on each side
+        dist += 400
+
+        lat0 = lat1 + math.cos((azimuth + 180)*math.pi/180) * 200 / 111.195
+        lon0 = lon1 + math.sin((azimuth + 180)*math.pi/180) * 200 / 111.195
+        latlon0 = (lat0, lon0)
+
+    binsize = paras["binsize"]
+    binwidth = paras["binwidth"]
+
+    nbins = int(dist / binsize + 0.5) + 1
+    dist = (nbins - 1) * binsize
+    bins = np.linspace(0, dist, nbins)
+    # print(bins)
+
+    profile_id = paras["profile_id"]
+    clip = paras["clip_percentage"] / 100.0
+
+    path = kwargs["io"]["outpath"]
+
+    depth_range = kwargs["plot"]["depth_range"]
+
+    # each stations
+    if latlon0 is None:
+        files = glob.glob(path + "/migration_1station/*.pkl")
+        files.sort()
+        for file in files:
+            stmig = read(file)
+            lat0 = stmig[0].stats.station_latitude - 0.5 * (bins[-1] + bins[0]) / 111.195
+            lon0 = stmig[0].stats.station_longitude
+            latlon0 = (lat0, lon0)
+            _plot_1station(stmig, latlon0, azimuth, bins, width=binwidth, clip=clip, savepath=path,
+                           depth_range=depth_range)
+    # profile
+    else:
+        wc = paras["wild_card"]
+        files = glob.glob(path + "/migration_1station/*%s*.pkl" % wc)
+        print("number of stations to stack: ", len(files))
+        stmig = read(path + "/migration_1station/*%s*.pkl" % wc)
+        _plot_stations(stmig, latlon0, azimuth, bins, width=binwidth, clip=clip, savepath=path,
+                       depth_range=depth_range, profile_id=profile_id)
+
+
+def _plot_1station(stmig, latlon0, azimuth, bins, width, clip, savepath, depth_range):
+    # get boxes for mig-stacking
+    boxes = get_profile_boxes(latlon0, azimuth=azimuth, bins=bins, width=width)
+
+    # depth and stack array
+    pos, depth, stack = profile(stream=stmig, boxes=boxes)
+
+    # the setting makes the station location in the center of the image
+    pos -= 0.5 * (bins[-1] - bins[0])
+
+    dist_range = [-100, 100]
+
+    # get station id
+    tr = stmig[0]
+    if tr.stats.location == "":
+        station_id = ".".join([tr.stats.network, tr.stats.station])
+    else:
+        station_id = ".".join([tr.stats.network, tr.stats.station, tr.stats.location])
+    print("Plotting - station id: ", station_id)
+
+    # latitude corresponding to pos
+    lats = []
+    for b in boxes:
+        # print(b["pos"], b["latlon"])
+        lats.append(b["latlon"][0])
+
+    extent = [np.min(pos), np.max(pos), np.min(depth), np.max(depth)]
+    # extent = [np.min(lats), np.max(lats), np.min(depth), np.max(depth)]
+
+    # normalization
+    stack /= np.max(np.abs(stack))
+
+    amp = np.sum(stack, axis=1)
+    amp /= np.max(np.abs(amp))
+
+    # print(extent)
+
+    # return amp, stack, extent
+    _plot(amp, depth, stack, extent, dist_range, depth_range, savepath, profile_id=station_id, clip=clip)
+
+
+def _plot_stations(stmig, latlon0, azimuth, bins, width, clip, savepath, depth_range, profile_id):
+    # get boxes for mig-stacking
+    boxes = get_profile_boxes(latlon0, azimuth=azimuth, bins=bins, width=width)
+
+    # depth and stack array
+    pos, depth, stack = profile(stream=stmig, boxes=boxes)
+
+    # the setting makes the station location in the center of the image
+    # pos -= 0.5*(bins[-1]-bins[0])
+
+    dist_range = [np.min(pos), np.max(pos)]
+
+    # get station id
+    # tr = stmig[0]
+    # if tr.stats.location == "":
+    #     station_id = ".".join([tr.stats.network, tr.stats.station])
+    # else:
+    #     station_id = ".".join([tr.stats.network, tr.stats.station, tr.stats.location])
+    print("Plotting - profile id: ", profile_id)
+
+    # latitude corresponding to pos
+    lats = []
+    for b in boxes:
+        # print(b["pos"], b["latlon"])
+        lats.append(b["latlon"][0])
+
+    extent = [np.min(pos), np.max(pos), np.min(depth), np.max(depth)]
+    # extent = [np.min(lats), np.max(lats), np.min(depth), np.max(depth)]
+
+    # normalization
+    stack /= np.max(np.abs(stack))
+
+    amp = np.sum(stack, axis=1)
+    amp /= np.max(np.abs(amp))
+
+    # print(extent)
+
+    # return amp, stack, extent
+    _plot(amp, depth, stack, extent, dist_range, depth_range, savepath, profile_id=profile_id, clip=clip)
+
+
+def _plot(amp, depth, stack, extent, dist_range, depth_range, savepath, profile_id, clip):
+    # init figure and axes
+    N = 256
+    rdbu_r = cm.get_cmap('RdBu_r', N)
+    newcolors = rdbu_r(np.linspace(0, 1, N))
+    white = np.array([255 / 255, 255 / 255, 255 / 255, 1])
+    a = int(N / 2 - N / 12)
+    b = int(N / 2 + N / 12)
+    newcolors[a:b, :] = white
+    newcmp = ListedColormap(newcolors)
+
+    fig, (ax2, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(4.5, 3), sharey=True,
+                                   gridspec_kw={'width_ratios': [0.5, 2]})
+    im = ax1.imshow(stack, interpolation='bilinear', aspect="auto",
+                    origin="lower", extent=extent,
+                    cmap=newcmp, vmin=-clip, vmax=clip)
+
+    ax1.yaxis.set_major_locator(MultipleLocator(50))
+    ax1.yaxis.set_minor_locator(MultipleLocator(10))
+    ax1.xaxis.set_minor_locator(MultipleLocator(50))
+    ax1.grid(which="both", axis="y", linestyle=":", linewidth=0.5)
+
+    ax1.annotate(profile_id,
+                 xy=(0.995, 0.995), xycoords='axes fraction',
+                 # xytext=(0.95, 0.05), textcoords=station_id,
+                 # arrowprops=dict(facecolor='black', shrink=0.05),
+                 horizontalalignment='right', verticalalignment='top')
+
+    ax1.set_ylim(depth_range)
+    ax1.set_xlim(dist_range)
+    ax1.invert_yaxis()
+    # ax1.invert_xaxis()
+
+    ax2.plot(amp, depth, linewidth=0.75, color="k")
+    ax2.fill_betweenx(depth, 0, amp, where=amp >= 0, facecolor='red')
+    ax2.fill_betweenx(depth, 0, amp, where=amp < 0, facecolor='blue')
+    ax2.grid(which="both", axis="y", linestyle=":", linewidth=0.5)
+    ax2.set_xlim([-0.1, 0.1])
+    ax2.set_xlabel("Amplitude")
+    ax2.set_ylabel("Depth [km]")
+
+    cbar = fig.colorbar(im, ax=ax1)
+    cbar.set_label("Amplitude")
+
+    ax1.set_xlabel("CRP Distance [km]")
+    # ax1.set_ylabel("Depth [km]")
+
+    plt.tight_layout()
+    path = savepath + "/figures"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    filen = path + "/mig_crp_" + profile_id
+    for fmt in [".pdf", ".png"]:
+        fn = filen + fmt
+        plt.savefig(fn)
+    plt.close()
 
 
